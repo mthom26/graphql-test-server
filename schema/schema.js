84d1e50cc99a578583b55cb82c80d471e8ce6e38
mthom26 <mthom2626@gmail.com>
2018-05-13 17:26:29 +0100

    Add Team Member query

--- schema/schema.js
+++ schema/schema.js
@@ -8,11 +8,26 @@ const testData = [
   { name: 'Task 3', id: '03' }
 ];
 
+const testDataMembers = [
+  { name: 'John Doe', title: 'Project Lead', id: 'tm01' },
+  { name: 'Jane Doe', title: 'Lead Developer', id: 'tm02' },
+  { name: 'John Doe Jr.', title: 'UI/UX Lead', id: 'tm03' }
+];
+
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString }
+  })
+});
+
+const TeamMemberType = new GraphQLObjectType({
+  name: 'TeamMember',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    title: { type: GraphQLString }
   })
 });
 
@@ -27,6 +42,13 @@ const RootQuery = new GraphQLObjectType({
 
         // filter returns array, so extract item from it, to return list use GraphQLList
         return testData.filter( d => d.id == args.id)[0];
+      }
+    },
+    teamMember: {
+      type: TeamMemberType,
+      args: { id: { type: GraphQLString}},
+      resolve(parent, args) {
+        return testDataMembers.filter( member => member.id == args.id)[0];
       }
     }
   }
